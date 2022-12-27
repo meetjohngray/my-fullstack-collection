a93cde7a85f8fe8f17c5e1ef4522e9911dc6ea7e
John Gray <john.edwin.gray@gmail.com>
2022-12-28 12:38:21 +1300

    join getQuotes to authors table

--- server/db/quotes.ts
+++ server/db/quotes.ts
@@ -1,7 +1,9 @@
 const connection = require('./connection')
 
 function getQuotes(db=connection){
-  return db('quotes').select()
+  return db('quotes')
+    .join('authors', 'authors.id', 'author_id')
+    .select('quotes.id as id', 'quotes.text', 'authors.name')
 }
 
 module.exports = {
