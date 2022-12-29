3c3562aede4701be4d45ad30554db9dbd9c4729f
John Gray <john.edwin.gray@gmail.com>
2022-12-30 11:02:36 +1300

    set-up server for quote routes and client side routing

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const path = require('path')
+const quoteRoutes = require('./routes/quotes')
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
+
+server.use('/quote', quoteRoutes)
+server.get('*', (req, res) => {
+  res.sendFile(path.resolve('server/public/index.html'))
+})
 
 module.exports = server
