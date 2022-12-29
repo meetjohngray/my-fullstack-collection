cf05cc1f25d131849a40d723774041ba99fd9ace
John Gray <john.edwin.gray@gmail.com>
2022-12-30 10:30:34 +1300

    getSingleQuote and getQuotesByAuthor completed and tested

--- server/db/__tests__/quotes.test.ts
+++ server/db/__tests__/quotes.test.ts
@@ -5,12 +5,6 @@ const testDb = knex(config)
 const db = require('../../db/quotes')
 const dbquote = require('../../db/data/quotes.json')
 
-interface quote {
-  id: string
-  author_id: number
-  text: string
-}
-
 beforeAll(() => {
   return testDb.migrate.latest()
 })
@@ -22,9 +16,32 @@ beforeEach(() => {
 describe('getQuotes', () => {
   it('returns the quotes array', () => {
     return db.getQuotes(testDb)
-      .then((quotes: quote[]) => {
+      .then((quotes: Quote[]) => {
         expect(quotes).toHaveLength(dbquote.length)
         expect(quotes[0].text).toContain('Dismissing an idea')
+      })
+  })
+})
+
+describe('getSingleQuote', () => {
+  it('returns the correct quote', () => {
+    const id = 3
+    return db.getSingleQuote(id, testDb)
+      .then((quote: JoinedQuote)=> {
+        expect(quote.text).toContain('things you make war with')
+        expect(quote.name).toContain('Chah')
+      })
+  })
+})
+
+describe('getQuotesByAuthor', () => {
+  it('returns all the quotes by a given author', () => {
+    const authId = 28
+    return db.getQuotesByAuthor(authId, testDb)
+      .then((quotes: JoinedQuote[]) => {
+        expect(quotes).toHaveLength(2)
+        expect(quotes[0].text).toContain('choose to listen')
+        expect(quotes[0].name).toContain('Abbey')
       })
   })
 })
