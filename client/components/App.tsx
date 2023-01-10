68c52b56a286ae13d9d5ff1a92a3c70499410401
Gerard Paapu <gerard@gerardpaapu.com>
2023-01-11 11:24:25 +1300

    fix: use correct (thunk) type for useAppDispatch

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -5,15 +5,14 @@ import { fetchQuotes } from '../actions/quoteActions'
 import Quote from './Quote'
 
 function App() {
-  
-  const quotes = useAppSelector(reduxState => reduxState.quotes)
+  const quotes = useAppSelector((reduxState) => reduxState.quotes)
   const dispatch = useAppDispatch()
   const [current, setCurrent] = useState(0)
 
   useEffect(() => {
     dispatch(fetchQuotes())
-  }, [])
-  
+  }, [dispatch])
+
   function nextQuote() {
     setCurrent(current === quotes.length - 1 ? 0 : current + 1)
   }
@@ -28,17 +27,12 @@ function App() {
         <h1>My Collection</h1>
       </header>
       <section className="main">
-        {quotes.map((item, index) => (
-          current === index && (
-            <Quote key = {item.id} quote = {item} />
-          )
-        ))}
-        <div onClick = {previousQuote}>
-          ⬅
-        </div>
-        <div onClick={nextQuote}>
-          ⮕
-        </div>
+        {quotes.map(
+          (item, index) =>
+            current === index && <Quote key={item.id} quote={item} />
+        )}
+        <div onClick={previousQuote}>⬅</div>
+        <div onClick={nextQuote}>⮕</div>
       </section>
     </>
   )
--- client/store.ts
+++ client/store.ts
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware  from 'redux-thunk'
+import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from '@redux-devtools/extension'
 import type { ThunkAction as BaseThunkAction } from 'redux-thunk'
 import type { AnyAction } from 'redux'
@@ -16,15 +16,15 @@ export type RootState = ReturnType<typeof store.getState>
 
 export type AppDispatch = typeof store.dispatch
 // <T = void> means that if the Promise<T> returns nothing, then the type is void
-export type ThunkAction<T = void > = BaseThunkAction<
-// The return type will be a Promise with a type designated by the generic
-Promise<T>,
-// State that it's attached to...
-RootState,
-// Extra thunk argument...
-void,
-// The action that comes from redux, expects it to be an object with a type property
-AnyAction
+export type ThunkAction<T = void> = BaseThunkAction<
+  // The return type will be a Promise with a type designated by the generic
+  Promise<T>,
+  // State that it's attached to...
+  RootState,
+  // Extra thunk argument...
+  void,
+  // The action that comes from redux, expects it to be an object with a type property
+  AnyAction
 >
 
 export default store
