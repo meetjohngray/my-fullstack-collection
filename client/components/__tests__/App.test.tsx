52ba06929c6176021614e900bac2dcfa74100c92
John Gray <john.edwin.gray@gmail.com>
2023-01-12 10:56:39 +1300

    App.test.tsx passes with async issues

---
+++ client/components/__tests__/App.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react'
+// can this be part of the boilerplate? Needed to use .toBeInTheDocument, etc?
+// Might require a jest set-up file
+import '@testing-library/jest-dom'
+import { Provider } from 'react-redux'
+import store from'../../store'
+
+import App from '../App'
+
+
+describe('App', () => {
+  it('displays the header', ()=> {
+     render(
+      <Provider store={store}>
+        <App />
+      </Provider>  
+      )
+  
+    const header = screen.getByRole('heading')
+    expect(header).toBeInTheDocument()
+    expect(header).toHaveTextContent(/quote this/i)
+  })
+})
--- client/reducers/quotes.ts
+++ client/reducers/quotes.ts
@@ -1,9 +1,9 @@
-import { SET_QUOTES, TQuoteAction } from '../actions/quoteActions'
+import { SET_QUOTES, QuoteAction } from '../actions/quoteActions'
 import { JoinedQuote } from '../../models/Iquotes'
 
 const initialState: JoinedQuote[] = []
 
-const reducer = (state = initialState, action: TQuoteAction) => {
+const reducer = (state = initialState, action: QuoteAction) => {
   const { type, payload } = action
   switch (type) {
     case SET_QUOTES:
